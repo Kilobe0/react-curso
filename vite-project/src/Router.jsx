e67a829242819306df44aa2682ca8bb8073e8cdf
Kilobe0 <104333874+Kilobe0@users.noreply.github.com>
2024-05-03 20:10:47 -0300

    Add PageNotFound component to handle 404 errors.

--- vite-project/src/Router.jsx
+++ vite-project/src/Router.jsx
@@ -1,6 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 
-import { Inicial, SobreNos } from "./pages";
+import { Inicial, SobreNos, PageNotFound } from "./pages";
 import { LayoutPadrao } from "./layouts";
 
 const Router = () => {
@@ -9,6 +9,7 @@ const Router = () => {
       <Route path="/" element={<LayoutPadrao />}>
         <Route path="/" element={<Inicial />} />
         <Route path="/sobre-nos" element={<SobreNos />} />
+        <Route path="*" element={<PageNotFound />} />
       </Route>
     </Routes>
   );
